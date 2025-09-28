bc9ba5247905f63bfb47c172cd85420392cad6d7
Bing Chilling <119095020+AlvinT-data@users.noreply.github.com>
2025-09-28 16:12:48 -0700

    Update qc_framework.sql

--- scripts/silver/qc_framework.sql
+++ scripts/silver/qc_framework.sql
@@ -1,6 +1,7 @@
 /*
 Used to check data quality of datable and decide what actions are needed to clean the data
 */
+
 -- Qulity Checks Framework
 
 -- Check Nulls or Duplicates in Primary Key
@@ -20,15 +21,31 @@ WHERE field != TRIM(field)
 
 -- Check for NULLS or Negative Numbers
 -- Expectation: No Results
-SELECT field
-FROM table_name
-WHERE field < 0 OR field IS NULL
+SELECT sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 OR sls_order_dt IS NULL
 
 -- Data Standardization & Consistency
 SELECT DISTINCT field
-FRPOM table_name
+FROM table_name
 
 -- Check for Invalidate Date Orders
 SELECT *
-FRIN table_name
-WHERE start_date > end_date
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt < sls_order_dt AND sls_order_dt < sls__dt
+
+-- Check for Invalid Dates
+SELECT
+	sls_ship_dt
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt <= 0 OR LEN(sls_ship_dt) != 8 OR sls_ship_dt IS NULL
+
+-- Check business rule e.g. sales != quantity * price
+SELECT DISTINCT
+	sales, 
+	quantity,
+	price
+FROM table
+WHERE sales != quantity * price OR 
+	sales IS NULL OR quantity IS NULL OR price IS NULL OR
+	sales = 0 OR quantity = 0 OR price = 0
